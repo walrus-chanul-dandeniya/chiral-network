c547139cb28395b563022daf824c501f6bf7495c
pandas-joy-zou <155475835+pandas-joy-zou@users.noreply.github.com>
2025-09-23 13:58:20 -0400

    Update Account.svelte

--- src/pages/Account.svelte
+++ src/pages/Account.svelte
@@ -17,6 +17,8 @@
   import { t, locale } from 'svelte-i18n'
   import { showToast } from '$lib/toast'
   import { get } from 'svelte/store'
+  import { wallet, transactions, totalEarned, totalSpent } from '$lib/stores';
+
   const tr = (k: string, params?: Record<string, any>) => get(t)(k, params)
   
   // Basic obfuscation for locally stored passwords. NOT for cryptographic security.
