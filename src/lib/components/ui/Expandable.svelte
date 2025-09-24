6a19391a131a1c6d541a3735b5a4bd6210460177
Fahim Jawad <fahim.jawad@stonybrook.edu>
2025-09-24 14:35:06 -0400

    feat(settings) added reactive UI for expandible/collapsible settings

---
+++ src/lib/components/ui/Expandable.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { slide } from "svelte/transition";
+  import { ChevronDown } from "lucide-svelte";
+
+  export let isOpen = false;
+</script>
+
+<div class="border rounded-md">
+  <button
+    on:click={() => (isOpen = !isOpen)}
+    class="flex items-center justify-between w-full p-4 text-left"
+  >
+    <div class="font-semibold">
+      <slot name="title" />
+    </div>
+    <ChevronDown
+      class="transform transition-transform duration-200 {isOpen
+        ? 'rotate-180'
+        : ''}"
+    />
+  </button>
+  {#if isOpen}
+    <div transition:slide={{ duration: 200 }} class="p-4 border-t">
+      <slot />
+    </div>
+  {/if}
+</div>
