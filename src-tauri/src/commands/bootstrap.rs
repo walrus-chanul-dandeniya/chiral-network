ba3fd7f6e9b551eb7a4493c00bae0b18007ec6b4
totoro-ming-lin <ming.lin@stonybrook.edu>
2025-11-05 21:41:12 -0500

    new bootstrap

--- src-tauri/src/commands/bootstrap.rs
+++ src-tauri/src/commands/bootstrap.rs
@@ -7,6 +7,8 @@ pub fn get_bootstrap_nodes() -> Vec<String> {
     vec![
         "/ip4/54.198.145.146/tcp/4001/p2p/12D3KooWNHdYWRTe98KMF1cDXXqGXvNjd1SAchDaeP5o4MsoJLu2"
             .to_string(),
+        "/ip4/136.116.190.115/tcp/4001/p2p/12D3KooWETLNJUVLbkAbenbSPPdwN9ZLkBU3TLfyAeEUW2dsVptr"
+            .to_string(),
     ]
 }
 
