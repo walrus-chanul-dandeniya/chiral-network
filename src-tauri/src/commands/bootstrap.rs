d0791bc43fab1eb2b63d88124360861f9eab2b05
totoro-ming-lin <ming.lin@stonybrook.edu>
2025-11-19 07:52:23 -0500

    Update bootstrap.rs

--- src-tauri/src/commands/bootstrap.rs
+++ src-tauri/src/commands/bootstrap.rs
@@ -7,7 +7,9 @@ pub fn get_bootstrap_nodes() -> Vec<String> {
     vec![
         "/ip4/134.199.240.145/tcp/4001/p2p/12D3KooWFYTuQ2FY8tXRtFKfpXkTSipTF55mZkLntwtN1nHu83qE"
             .to_string(),
-        "/ip4/136.116.190.115/tcp/4001/p2p/12D3KooWETLNJUVLbkAbenbSPPdwN9ZLkBU3TLfyAeEUW2dsVptr"
+        "/ip4/104.198.62.217/tcp/4001/p2p/12D3KooWETLNJUVLbkAbenbSPPdwN9ZLkBU3TLfyAeEUW2dsVptr"
+            .to_string(),
+        "/ip4/104.198.62.217/tcp/4002/p2p/12D3KooWGV5BUSYMhNMrhdPh9EUbuLrvAiDsMXEMRpGGvt4LQneA" // relay-capable peer
             .to_string(),
         "/ip4/130.245.173.105/tcp/4001/p2p/12D3KooWGFRvjXFBoU9y6xdteqP1kzctAXrYPoaDGmTGRHybZ6rp"
             .to_string(),
